3b047ddecea5d456030d079c29ea0bde1547aa57
theerapat.demo <theerapat.demo@gmail.com>
2025-08-25 12:49:01 +0700

    update test script

--- tests/employee.spec.ts
+++ tests/employee.spec.ts
@@ -1,15 +1,13 @@
-import { test, expect } from '@playwright/test'
-import { ENV_BASE } from '../config/env'
+import { test, expect } from "@playwright/test";
+import { ENV_BASE } from "../config/env";
 
-test.describe('Test employee service', () => {
-    test('Check data return for employees', async ({ request }) => {
-        const res = await request.get(`${ENV_BASE.SERVICE_NAMES_QUERY_EMPLOYEES}`,
-            {
-                headers: {
-                    'Accept': 'application/json'
-                }
-            }
-        );
+test.describe("Test employee service", () => {
+    test("Check data return for employees", async ({ request }) => {
+        const res = await request.get(`${ENV_BASE.SERVICE_NAMES_QUERY_EMPLOYEES}`, {
+            headers: {
+                Accept: "application/json",
+            },
+        });
 
         const jsonData = await res.json();
         expect(Array.isArray(jsonData.data)).toBe(true);
@@ -20,26 +18,29 @@ test.describe('Test employee service', () => {
                 email: expect.stringMatching(/^[^@\s]+@[^@\s]+\.[^@\s]+$/),
                 salary: expect.any(String),
                 department_name: expect.any(String),
-                hire_date: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)
+                hire_date: expect.stringMatching(
+                    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/
+                ),
             });
         }
     });
 
-    test('health check', async ({ request }) => {
-        const res = await request.get(`${ENV_BASE.SERVICE_NAMES_HEALTH_CHECK}`,
-            {
-                headers: {
-                    'Accept': 'application/json'
-                }
-            }
-        );
+    test("health check", async ({ request }) => {
+        const res = await request.get(`${ENV_BASE.SERVICE_NAMES_HEALTH_CHECK}`, {
+            headers: {
+                Accept: "application/json",
+            },
+        });
+        
         expect(res.status()).toBe(200);
         const jsonData = await res.json();
         expect(jsonData).toMatchObject({
-            timestamp: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/),
+            timestamp: expect.stringMatching(
+                /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/
+            ),
             uptime: expect.any(Number),
             environment: "development",
-            version: "1.0.0"
-        })
+            version: "1.0.0",
+        });
     });
 });
